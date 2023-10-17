e097129a2b17f9d4b77a21d45aae63e588a279c5
Fritz Guzman <fritz.developer10@gmail.com>
2023-10-17 15:46:00 -0500

    nav

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,71 @@
+import { NavLink } from "react-router-dom"
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+        <ul className="flex items-center gap-3">
+            <li className="font-semibold text-lg">
+                <NavLink to = '/'>
+                    Shop
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/all'>
+                    All
+                </NavLink>
+            </li>
+            <li> 
+                <NavLink to = '/clothes'>
+                    Clothes
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/electronics'>
+                    Electronics
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/furnitures'>
+                    Furnitures
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/toys'>
+                    Toys
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/others'>
+                    Others
+                </NavLink>
+            </li>
+        </ul>
+        <ul className="flex items-center gap-3">
+            <li>
+                <NavLink to = '/'>
+                    Shop
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/my-orders'>
+                    My Orders
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/my-account'>
+                    My Account
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to = '/sign-in'>
+                    Sign In
+                </NavLink>
+            </li>
+            <li>
+                🛒 0
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
