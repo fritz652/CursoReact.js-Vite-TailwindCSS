1c843bdb54d68705e328c34e8c61525cb0574b67
Fritz Guzman <fritz.developer10@gmail.com>
2023-12-10 20:02:29 -0500

    reapsando

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,9 +4,20 @@ export const ShoppingCartContext = createContext();
 
 const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const openProductDetail = () => setIsProductDetailOpen(true);
+  const clouseProductDetail = () => setIsProductDetailOpen(false);
   console.log("COUNT", count);
   return (
-    <ShoppingCartContext.Provider value={{ count, setCount }}>
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+        openProductDetail,
+        clouseProductDetail,
+        isProductDetailOpen,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
