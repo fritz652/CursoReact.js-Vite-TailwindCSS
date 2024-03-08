00312e2e20dcd7be99ac7ca8aa1aa3645bcecb3b
Fritz Guzman <fritz.developer10@gmail.com>
2024-03-07 20:39:01 -0500

    my orders

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,12 +2,19 @@ import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
 import OrderCard from "../../Components/OrderCard";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const MyOrder = () => {
   const context = useContext(ShoppingCartContext);
   return (
     <Layout>
-      MyOrder
+      <div className="flex items-center justify-center relative w-80 mb-6">
+        <Link to="/my-orders" className="absolute left-0">
+          <ChevronLeftIcon className="h-6 text-black -cursor-pointer" />
+        </Link>
+        <h1>My Order</h1>
+      </div>
       <div className="flex flex-col w-80">
         {context.order?.slice(-1)[0].products.map((product) => (
           <OrderCard
