3ba86c3b482e8698261b1229ccd7f316a87af829
Fritz Guzman <fritz.developer10@gmail.com>
2024-04-07 23:56:56 -0500

    shot to Fritz

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
       <ul className="flex items-center gap-3">
         <li className="font-semibold text-lg">
-          <NavLink to="/">Shop</NavLink>
+          <NavLink to="/">Fritz</NavLink>
         </li>
         <li>
           <NavLink
